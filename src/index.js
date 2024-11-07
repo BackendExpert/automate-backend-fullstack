15420af74dc1084abccbcc92161cae0528e55d12
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-07 14:14:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -91,10 +91,6 @@ const AuthController = require('../Controllers/AuthController');
 
 const router = express.Router();
 
-router.post('/SignUp', AuthController.SignUp)
-router.post('/SignIn', AuthController.Login)
-router.get('/GetUsername/:email', AuthController.CurrentUserName)
-
 module.exports = router;
 
     `;
