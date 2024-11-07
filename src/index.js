889a860765825fd3fea3096a04e7052ebb3f9a78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-11-07 14:13:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -83,9 +83,19 @@ console.log(`${ControllerName} Controller created successfully! Route will be Cr
 
 }
 
-async function(ModelName){
+async function CreateRoute (ModelName){
     const ControllerConect = `
-        
+
+const express = require('express');
+const AuthController = require('../Controllers/AuthController');
+
+const router = express.Router();
+
+router.post('/SignUp', AuthController.SignUp)
+router.post('/SignIn', AuthController.Login)
+router.get('/GetUsername/:email', AuthController.CurrentUserName)
+
+module.exports = router;
 
     `;
 }
